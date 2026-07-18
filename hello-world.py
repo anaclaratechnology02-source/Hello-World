f40306c72cdbaec9fedb8eec32b8e663d97ce0d5
Ana Clara <anaclaratechnology02@gmail.com>
2026-07-18 12:25:56 -0300

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,11 @@
+print('Welcome! We want to get to know you')
+name = input('What is your name?')
+age = input('How old are you?')
+country = input('where are you from?')
+n1 = int( input('Digite o primeiro número'))
+n2 = int( input('Digite outro número'))
+s = n1 + n2
+print('a soma entre {} e {} é igual a {}'.format(n1,n2, s))
+
+
 
