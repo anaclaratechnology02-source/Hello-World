37e33bf21e8e3107ecc6c66a49430ac52ba2d9de
Ana Clara <anaclaratechnology02@gmail.com>
2026-08-11 20:40:43 -0300

    Create varáveis.py

---
+++ varáveis.py
@@ -0,0 +1,36 @@
+a = input("Digite algo")
+Digite algo 400
+print("O tipo primitivo desse valor é", type(a))
+O tipo primitivo desse valor é <class 'str'>
+print("só tem espaço?", a.isspace())
+só tem espaço? False
+print("è um número?", a.isnumeric())
+è um número? False
+print("é alfabético", a.isalpha())
+é alfabético False
+print('é alfanumério?', a.isalnum())
+é alfanumério? False
+print('é um número?', a.isnumeric())
+é um número? False
+print('está em maíúsculo?', a.islower())
+está em maíúsculo? False
+a = input('digite algo')
+digite algo Sol123
+print("O tipo primitivo desse valor é", type(a))
+O tipo primitivo desse valor é <class 'str'>
+print("só tem espaço?", a.isspace())
+só tem espaço? False
+print("è um número?", a.isnumeric())
+è um número? False
+print('é alfanumério?', a.isalnum())
+é alfanumério? False
+a = input("Digite algo")
+Digite algo400
+print("è um número?", a.isnumeric())
+è um número? True
+a = input('digite algo')
+digite algosol1234R
+print('está em maíúsculo?', a.islower())
+está em maíúsculo? False
+print('é alfanumério?', a.isalnum())
+é alfanumério? True
